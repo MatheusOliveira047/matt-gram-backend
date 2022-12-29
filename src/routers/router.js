401e778bd6c478565b1d38f5515290952063d956
MatheusOliveira047 <ludsonmatheus@gmail.com>
2022-12-28 23:29:05 -0300

    criando models do usuário

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
 
-router.get('/',(req,res)=>{res.status(200).json({mmsg:"Deu certo"})})
+router.get('/',(req,res)=>{res.status(200).json({msg:"Deu certo"})})
 
 module.exports = router
