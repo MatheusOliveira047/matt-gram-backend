93a42ad81a77c23b5f81c2aef62bc6519e65f196
MatheusOliveira047 <ludsonmatheus@gmail.com>
2022-12-30 19:40:52 -0300

    buscando usuário por id

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -112,9 +112,23 @@ const update = async(req,res)=>{
 
 }
 
+const getUserById = async(req,res)=>{
+  const {id} = req.params
+
+  try {
+    const user = await User.findById(mongoose.Types.ObjectId(id)).select("-password")
+    return res.status(200).json(user)
+  } catch (error) {
+    res.status(404).json({errors:["Usuário não encontrado"]})
+
+  }
+
+}
+
 module.exports = {
   register,
   login,
   getCurrentUser,
-  update
+  update,
+  getUserById
 }
--- src/routers/UserRoutes.js
+++ src/routers/UserRoutes.js
@@ -1,5 +1,11 @@
 const router = require('express').Router()
-const {register,login,getCurrentUser,update} = require("../controllers/UserController")
+const {
+  register,
+  login,
+  getCurrentUser,
+  update,
+  getUserById
+} = require("../controllers/UserController")
 
 
 const validate = require("../middlewares/handleValidation")
@@ -15,5 +21,6 @@ router.post("/register", userCreateValidation(), validate, register);
 router.post('/login', loginValidation(),validate,login)
 router.get('/profile', authGuard,getCurrentUser)
 router.put("/",authGuard,userUpdateValidation(),validate, imageUpload.single("profileImage"),update)
+router.get("/:id",getUserById)
 
 module.exports = router
