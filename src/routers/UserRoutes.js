4cada3871bdf46cc8af2975750bb5ec74c41eaa8
MatheusOliveira047 <ludsonmatheus@gmail.com>
2022-12-30 11:12:42 -0300

    middleware do atualização de usuário

--- src/routers/UserRoutes.js
+++ src/routers/UserRoutes.js
@@ -1,13 +1,19 @@
 const router = require('express').Router()
-const {register,login,getCurrentUser} = require("../controllers/UserController")
+const {register,login,getCurrentUser,update} = require("../controllers/UserController")
 
 
 const validate = require("../middlewares/handleValidation")
-const {userCreateValidation,loginValidation} = require("../middlewares/userValidations")
+const {imageUpload} = require("../middlewares/imageUploads")
+const {
+  userCreateValidation,
+  loginValidation,
+  userUpdateValidation
+} = require("../middlewares/userValidations")
 const authGuard = require("../middlewares/authGuard")
 
 router.post("/register", userCreateValidation(), validate, register);
 router.post('/login', loginValidation(),validate,login)
 router.get('/profile', authGuard,getCurrentUser)
+router.put("/",authGuard,userUpdateValidation(),validate, imageUpload.single("profileImage"),update)
 
 module.exports = router
