74cc0013d67a77c64a928bbdf3136e71e22cd410
MatheusOliveira047 <ludsonmatheus@gmail.com>
2022-12-30 00:06:29 -0300

    criando validação de autenticação

--- src/routers/UserRoutes.js
+++ src/routers/UserRoutes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router()
-const {register,login} = require("../controllers/UserController")
+const {register,login,getCurrentUser} = require("../controllers/UserController")
 
 
 const validate = require("../middlewares/handleValidation")
 const {userCreateValidation,loginValidation} = require("../middlewares/userValidations")
+const authGuard = require("../middlewares/authGuard")
 
 router.post("/register", userCreateValidation(), validate, register);
 router.post('/login', loginValidation(),validate,login)
+router.get('/profile', authGuard,getCurrentUser)
 
 module.exports = router
