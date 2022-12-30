53539a575193768a91ce3f7cb70c46f646735a88
MatheusOliveira047 <ludsonmatheus@gmail.com>
2022-12-29 22:45:55 -0300

    criando usuário no banco de dados

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -14,7 +14,34 @@ const genereteToken = (id)=>{
 
 // Register user and sign in 
 const register = async(req,res)=>{
-  res.send("Registro")
+  const {name,email,password} = req.body
+
+  const user = await User.findOne({email})
+
+  if(user){
+    res.status(422).json({errors:["Por favo, ultilize outro e-mail"]})
+    return
+  }
+
+  const salt = await bcrypt.genSalt()
+  const passwordHash = await bcrypt.hash(password,salt)
+
+  const newUser = await User.create({
+    name,
+    email,
+    password:passwordHash
+  })
+
+  if(!newUser){
+    res.status(422).json({errors:['Houve um erro, por favor tente mais tarde']})
+    return
+  }
+
+  return res.status(201).json({
+    _id: newUser._id,
+    token: genereteToken(newUser._id),
+  })
+
 }
 
 
